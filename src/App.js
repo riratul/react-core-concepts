7ca883ee957b3cdffc0a185d3ac97ac0e7dd3961
rayatuli <rayatislam@gmail.com>
2020-03-28 21:24:18 +0100

    component Props

--- src/App.js
+++ src/App.js
@@ -13,8 +13,22 @@ function App() {
     backgroundColor:"yellow"
   }
 
-  const peoples = ["Rafat", "Kamal"]
+  const peoples = ["Rafat", "Kamal", "zaman","Sumon"]
+  const products = [
+    {name: "Photoshop",price: "$1999.99"},
+    {name: "illustrator",price: "$1000.00"},
+    {name: "PDF Reader",price: "$500.00"},
+    {name: "PDF Reader",price: "$500.00"},
+    {name: "PDF Reader",price: "$500.00"}
+  ]
 
+  const productNames = products.map(pro => pro.name);  // map() creates an array with property of an object inside an array
+
+  console.log("Product Names: ",productNames);
+
+  const peopleNames = peoples.map(ppl => ppl);
+
+  console.log("Peoples: ", peopleNames);
 
   return (
     <div className="App">
@@ -23,11 +37,32 @@ function App() {
         <p>
           Edit Done and save to reload.
         </p>
+        <ul>
+          {
+            peoples.map(ppl => <li>{ppl}</li>)
+          }
+        </ul>
+
+        <ol>
+          {
+              products.map(product => <li>{product.name}</li>)
+
+          }
+        </ol>
+        <h1>mapping product array then showing all the items of the array using the style of Products function</h1> 
+          {
+
+            products.map(pd => <Product p={pd}></Product>)
+
+          }
+
       <h1 style={style}>Name: {person.name + " " + person.phone}</h1>
        <p>My first React Paragraph</p>
 
-       <h2 style={{color:"white",backgroundColor:"black", padding:"10px"}}>Arlin</h2>
+       <h2 style={{color:"white",backgroundColor:"black", padding:"10px"}}>Showing Products Statically </h2>        
 
+       <Product p={products[0]}></Product>
+       <Product p={products[1]}></Product>
        <Person name="Rubel" age="23"></Person>
        <Person name={peoples[0]} age="30"></Person>
 
@@ -41,8 +76,7 @@ function Person(props){
       border:'2px solid red', 
       margin:'10px'
 
-    }
-    
+    }  
 
 
     console.log(props)
@@ -55,5 +89,35 @@ function Person(props){
 
     )
 }
+
+function Product(props){
+  const productStyles={
+    color:'red',
+    border:'1px solid',
+    borderRadius:'10px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left',
+    padding:'10px'
+  }
+
+    // const {name,price} = props.product;   // optional destructuring 
+
+
+
+  return(
+    <div style={productStyles}>
+        <h2>{props.p.name}</h2>      
+
+        <h1>{props.p.price}</h1>
+        <button>Buy now</button>
+    </div>
+  )
+
+
+}
+
+
 
 export default App;
